b4b396bbea9591ed60484533dff06f96ff901cb4
John Sherman <JShermanK1@gmail.com>
2023-04-11 14:29:13 -0400

    working 6 sec

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,177 @@
-use noodles::bcf::header::{StringMaps};
-use noodles::bcf::{self};
-use noodles::vcf::Header;
+use noodles::{
+    bcf::{self, header::StringMaps},
+    vcf::Header
+};
 use polars::prelude::*;
-use std::error::Error;
-use std::fs::File;
-use std::io::BufReader;
-use std::iter::repeat;
+use std::{
+    error::Error,
+    fs::File,
+    iter::repeat,
+    path::PathBuf,
+};
 use itertools::Itertools;
-use noodles::csi::{self};
+use noodles::csi;
 use rayon::prelude::*;
+use clap::{
+    Command, 
+    arg, 
+    value_parser
+};
+
+
+fn cli() -> Command {
+    Command::new("infochallenge")
+            .args(&[
+                arg!(
+                    -i --in <FILE> "Path to bcf file with index"
+                ).required(true)
+                 .value_parser(value_parser!(PathBuf)),
+            ])
+}
+
 
 fn main() -> Result<(), Box<dyn Error>> {
-    
-    let f = File::open("/gpfs0/scratch/mvc002/info_challenge.bcf")?;
-    let mut bcf_r = bcf::Reader::new(f);
+    let matches = cli().get_matches();
+    let in_path = matches.get_one::<PathBuf>("in").expect("error parsing input path");
+    let mut index_path = in_path.clone();
+    index_path.set_extension("bcf.csi");
 
-    bcf_r.read_file_format()?;
-    let r_header = bcf_r.read_header()?;
-    let header: Header = r_header.parse()?;
-    let _stringmaps: StringMaps = r_header.parse()?;
 
-    let mut records = bcf_r.records().peekable();
-   
-
-    let mut index_r = File::open("/gpfs0/scratch/mvc002/info_challenge.bcf.csi").map(csi::Reader::new)?;
+    let mut index_r = File::open(index_path).map(csi::Reader::new)?;
     let index = index_r.read_index()?;
-    let num_records = index.reference_sequences().par_iter().map(|refseq| {
-        let meta_o =  refseq.metadata();
-        if let Some(meta) = meta_o {
-            meta.mapped_record_count()
-        } else {
-            0
-        }
-    }).reduce(|| 0,|a,b| a + b );
 
-    println!("{:?}", num_records);
+    let num_samples;
+    let stringmaps: StringMaps;
+    let header: Header;
 
+    {
+        let f = File::open(in_path)?;
+        let mut bcf_r = bcf::Reader::new(f);
 
+        bcf_r.read_file_format()?;
+        let r_header = bcf_r.read_header()?;
+        header = r_header.parse()?;
+        stringmaps = r_header.parse()?;
+
+        let mut records = bcf_r.records().peekable();
+
+        let first_record = records.peek().unwrap().clone();
+        num_samples = first_record.as_ref().unwrap().genotypes().len();
+    }
+
+    let names_top = header.sample_names()
+                          .iter()
+                          .join("\t");
+
+    println!("{names_top}");
+
+    let data = (0..).map_while(|i|{
+        stringmaps.contigs().get_index(i)
+                        }).collect::<Vec<&str>>()
+                        .into_par_iter()
+                          .map(|chrom| {
+
+        let region = format!("{chrom}").parse().expect("failed to parse region");
+
+        let f = File::open("/gpfs0/scratch/mvc002/info_challenge.bcf").expect("failed to open file");
+        let mut bcf_r = bcf::Reader::new(f);
+
+        bcf_r.read_file_format().expect("failed to read format");
+        let _r_header = bcf_r.read_header().expect("failed to read header");
+
+        let records = bcf_r.query(stringmaps.contigs(), &index, &region)
+                           .expect("failed to query index");
+        
+
+        
+        //preallocate vectors to store data.
+        let mut samples: Vec<Vec<Option<i8>>> = (0..num_samples).map(|_| {
+            Vec::with_capacity(35e5 as usize) // a random guess from looking at the data
+        }).collect();
+
+        records.for_each(|record| {
+            let record = record.expect("failed to read record");
+            let gt = record.genotypes().as_ref();
+            //3 header values then genotypes are pairs
     
-    let mut samples: Vec<Vec<Option<i8>>> = (0..29).map(|_| {
-        Vec::with_capacity(num_records as usize)
-    }).collect();
-
-    let first_record = records.peek().unwrap().clone();
-    let num_samples = first_record.as_ref().unwrap().genotypes().len();
+            //get only the GT array, drop other data
+            let record_vals = gt[3..(num_samples * 2 + 3)].chunks_exact(2)
+                                    .map(|chunk| (chunk[0] + chunk[1]) as i8) //combine both alelles into one number
+                                    .enumerate();
+            //optimizes out bounds checking
+            assert!(samples.len() == record_vals.len());
+            record_vals.for_each(|(i, read_value)| {
     
+                    //map allels to helpfull numbers otherwise replace with None
+                    let value = match read_value { 
+                        0 => None,
+                        4 => Some(0_i8),
+                        6 => Some(1_i8),
+                        8 => Some(2_i8),
+                        _ => None
+                    };
+                    samples[i].push(value);
+              });
+        });
 
-    records.for_each(|record| {
-        let record = record.expect("failed to read record");
-        let gt = record.genotypes().as_ref();
-        //3 header values then genotypes are pairs
 
-        //get only the GT array, drop other data
-        let record_vals = gt[3..(num_samples * 2 + 3)].chunks_exact(2)
-                                .map(|chunk| (chunk[0] + chunk[1]) as i8) //combine both alelles into one number
-                                .enumerate();
-        assert!(samples.len() == record_vals.len());
-        record_vals.for_each(|(i, read_value)| {
-
-                let value = match read_value { //
-                    0 => None,
-                    4 => Some(0_i8),
-                    6 => Some(1_i8),
-                    8 => Some(2_i8),
-                    _ => None
-                };
-                samples[i].push(value);
-          });
-    });
-
-    /*samples.iter().for_each(|v| {
-        println!("{:#?}", v);
-    });*/
-    println!("finished reading");              
-
-    let data = samples.into_par_iter()
+        let data = samples.into_par_iter()
                 .map(|sample| {
                     let sample = sample.into_iter();
                     ChunkedArray::<Int8Type>::from_iter(sample)
                 })
                 .collect::<Vec<ChunkedArray<Int8Type>>>();
+        
+        data
+    }).reduce(
+        || (0..num_samples).map(|_| ChunkedArray::<Int8Type>::default())
+                            .collect::<Vec<ChunkedArray<Int8Type>>>(),
+        |a_s, b_s| {
+            a_s.into_iter()
+             .zip(b_s)
+             .map(|(mut a, b)| {
+                a.append(&b);
+                a
+             }).collect::<Vec<ChunkedArray<Int8Type>>>()
+        }
+    );             
+
     let data = Arc::new(data);
 
-    println!("{}", data[0].len());
+    //preinitiallize matrix with 0.0 
     let mut matrix = (0_usize..num_samples).map(|_| repeat(0.0).take(num_samples).collect())
                                  .collect::<Vec<Vec<f64>>>();
     
-    (0_usize..num_samples).combinations(2)
-                        .collect::<Vec<Vec<usize>>>()
+    //returns all combinations of all numbers in the sequence as tuples
+    (0_usize..num_samples).tuple_combinations::<(usize, usize)>()
+                        .collect::<Vec<(usize, usize)>>()
                         .into_par_iter()
-                        .map(|indicies| {
-                            let mask = &data[indicies[0]].is_not_null() & &data[indicies[1]].is_not_null();
-                            let data1 = &data[indicies[0]].filter(&mask).unwrap();
-                            let data2 = &data[indicies[1]].filter(&mask).unwrap();
+                        .map(|(i, j)| {
+
+                            let mask = &data[i].is_not_null() & &data[j].is_not_null();
+                            let data1 = &data[i].filter(&mask).unwrap();
+                            let data2 = &data[j].filter(&mask).unwrap();
                             let num_compares = (data1.len() * 2) as f64;
-                            //println!("{}", data1.filter(&data1.equal(4).unwrap()).unwrap().len());
+
                             let diff: f64 = (data1 - data2).abs().cast(&DataType::Float64).unwrap().sum().unwrap();
                             diff / num_compares
+
                         }).collect::<Vec<f64>>()
                         .into_iter()
-                        .zip((0_usize..num_samples).combinations(2).collect::<Vec<Vec<usize>>>())
-                        .for_each(|(distance, indecies): (f64, Vec<usize>)| {
-                            matrix[indecies[1]][indecies[0]] = distance;
+                        .zip((0_usize..num_samples).tuple_combinations::<(usize, usize)>())
+                        .for_each(|(distance, (i, j))| {
+                            //store values in the matrix in both directions
+                            matrix[j][i] = distance;
+                            matrix[i][j] = distance;
                         });
-    matrix.iter().for_each(|line| println!("{:?}", line));
+
+    matrix.iter()
+          .zip(header.sample_names().iter())
+          .for_each(|(line, samp_name)| {
+            let line = line.iter()
+                           .join("\t");
+            println!("{samp_name}\t{line}");
+        });
 
     Ok(())
 }
